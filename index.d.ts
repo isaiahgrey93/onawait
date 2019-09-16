6ae3ed07bbcb7928cf5990b40fb50606190348cb
Isaiah Grey <isaiah@echobind.com>
2019-09-16 13:46:34 -0700

    chore(typings): update typings file

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
-declare type AsyncAction = Promise<any> | ((...args: any[]) => Promise<AsyncResult>);
-declare type AsyncResult = {
+declare type InferPromise<T> = T extends Promise<infer R> ? R : never;
+declare type AsyncAction<T = any> = Promise<T> | ((...args: T[]) => Promise<AsyncResult>);
+declare type AsyncResult<T = any> = {
     error?: any;
-    response?: any;
+    response?: InferPromise<AsyncAction<T>>;
 };
-export declare const onawait: (action: AsyncAction) => Promise<AsyncResult>;
+export declare function onawait<T extends Promise<any>>(action: T): Promise<AsyncResult<T>>;
 export default onawait;
